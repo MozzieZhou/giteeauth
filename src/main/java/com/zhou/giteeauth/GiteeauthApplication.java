456cafabc310766c4493171006d672c631f83225
mozzie <mozzie.zhou@gmail.com>
2021-04-21 00:47:39 +0800

    init commit

---
+++ src/main/java/com/zhou/giteeauth/GiteeauthApplication.java
@@ -0,0 +1,13 @@
+package com.zhou.giteeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteeauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiteeauthApplication.class, args);
+	}
+
+}
